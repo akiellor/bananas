7c6928739b75b67c054e60889634dd399da83dd5
Andrew Kiellor <akiellor@gmail.com>
2015-08-21 16:36:22 -0700

    Birthday

---
+++ test/system_spec.js
@@ -0,0 +1,43 @@
+var scenarioBuilder = require(__dirname + '/../lib/scenario_builder');
+var expect = require('chai').expect;
+
+describe('system', function() {
+  var transitions = [
+    {
+      name: 'push empty',
+      requires: {},
+      provides: {full: true},
+      apply: function(system) {
+        system.push(1);
+      }
+    },
+    {
+      name: 'pop',
+      requires: {full: true},
+      provides: {full: false},
+      apply: function(system) {
+        system.pop();
+      }
+    }
+  ];
+
+  var verifications = [
+    {
+      name: 'empty',
+      requires: {full: false},
+      apply: function(system) {
+        expect(system.length).to.equal(0);
+      }
+    },
+    {
+      name: 'full',
+      requires: {full: true},
+      apply: function(system) {
+        expect(system.length).to.equal(1);
+      }
+    }
+  ];
+
+  scenarioBuilder(transitions, verifications).build(global, function() { return []; });
+});
+
