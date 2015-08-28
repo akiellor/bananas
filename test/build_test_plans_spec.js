0f2e0c68fbad017376105277fdef38b2cd36a8de
Andrew Kiellor <akiellor@gmail.com>
2015-08-28 07:57:39 -0700

    Refactor scenario builder to use new graph stuff.

---
+++ test/build_test_plans_spec.js
@@ -0,0 +1,32 @@
+var expect = require('chai').expect;
+
+describe('build test plans', function() {
+  it("should build no plans", function() {
+    var testPlans = buildTestPlans([]);
+
+    expect(testPlans).to.deep.equal([]);
+  });
+
+  it("should build for non-cyclical graph", function() {
+    function createNode(i) {
+      return {name: String(i)};
+    }
+    var i = 0;
+    var nodes = Array(7).join().split('').map(function(){return createNode(i++);});
+    nodes[0].children = [nodes[1], nodes[3]];
+    nodes[1].children = [nodes[3]];
+    nodes[3].children = [nodes[4]];
+    nodes[4].children = [nodes[1],nodes[2]];
+
+    var testPlans = buildTestPlans(nodes);
+
+    expect(testPlans[0]).to.deep.equal([
+      nodes[0],
+      nodes[1],
+      nodes[3],
+      nodes[4],
+      nodes[2]
+    ]);
+  });
+});
+
--- test/system_spec.js
+++ test/system_spec.js
@@ -1,44 +1,41 @@
 var scenarioBuilder = require(__dirname + '/../lib/scenario_builder');
 var expect = require('chai').expect;
 
-describe('system', function() {
-  var transitions = [
-    {
-      name: 'push empty',
-      requires: {},
-      provides: {full: true},
-      apply: function(system) {
-        system.push(1);
-      }
-    },
-    {
-      name: 'pop',
-      requires: {full: true},
-      provides: {full: false},
-      apply: function(system) {
-        system.pop();
-      }
+var transitions = [
+  {
+    name: 'push empty',
+    requires: {},
+    provides: {full: true},
+    apply: function(system) {
+      system.push(1);
     }
-  ];
-
-  var verifications = [
-    {
-      name: 'empty',
-      requires: {full: false},
-      apply: function(system) {
-        expect(system.length).to.equal(0);
-      }
-    },
-    {
-      name: 'full',
-      requires: {full: true},
-      apply: function(system) {
-        expect(system.length).to.equal(1);
-      }
+  },
+  {
+    name: 'pop',
+    requires: {full: true},
+    provides: {full: false},
+    apply: function(system) {
+      system.pop();
     }
-  ];
+  }
+];
 
-  var builder = scenarioBuilder(transitions, verifications);
-  builder.build(global, function() { return []; });
-});
+var verifications = [
+  {
+    name: 'empty',
+    requires: {full: false},
+    apply: function(system) {
+      expect(system.length).to.equal(0);
+    }
+  },
+  {
+    name: 'full',
+    requires: {full: true},
+    apply: function(system) {
+      expect(system.length).to.equal(1);
+    }
+  }
+];
 
+var builder = scenarioBuilder(transitions, verifications);
+builder.build(global, function() { return []; });
