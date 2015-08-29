a958c6b16ebb0876dfc23f829da734f5a8b75d2a
Pradheep <rpradhee@thoughtworks.com>
2015-08-28 18:05:17 -0700

    Adding more comprehensive tests

--- test/system_spec.js
+++ test/system_spec.js
@@ -1,41 +1,96 @@
 var scenarioBuilder = require(__dirname + '/../lib/scenario_builder');
 var expect = require('chai').expect;
 
+function sut() {
+  var state = [false, false, false];
+
+  return {
+    flip: function(index) {
+      state[index] = !state[index];
+    },
+    toString: function() {
+      return state.join('');
+    }
+  };
+}
+
 var transitions = [
   {
-    name: 'push empty',
+    name: 'flip 0 from initial',
     requires: {},
-    provides: {full: true},
+    provides: {first: 1},
     apply: function(system) {
-      system.push(1);
+      system.flip(0);
     }
   },
   {
-    name: 'pop',
-    requires: {full: true},
-    provides: {full: false},
+    name: 'flip 0 from 1',
+    requires: {first: 1},
+    provides: {first: 0},
     apply: function(system) {
-      system.pop();
+      system.flip(0);
+    }
+  },
+  {
+    name: 'flip 0 from 0',
+    requires: {first: 0},
+    provides: {first: 1},
+    apply: function(system) {
+      system.flip(0);
+    }
+  },
+  {
+    name: 'flip 1 from initial',
+    requires: {},
+    provides: {second: 1},
+    apply: function(system) {
+      system.flip(1);
+    }
+  },
+  {
+    name: 'flip 1 from 1',
+    requires: {second: 1},
+    provides: {second: 0},
+    apply: function(system) {
+      system.flip(1);
+    }
+  },
+  {
+    name: 'flip 1 from 0',
+    requires: {second: 0},
+    provides: {second: 1},
+    apply: function(system) {
+      system.flip(1);
+    }
+  },
+  {
+    name: 'flip 2 from initial',
+    requires: {},
+    provides: {third: 1},
+    apply: function(system) {
+      system.flip(2);
+    }
+  },
+  {
+    name: 'flip 2 from 1',
+    requires: {third: 1},
+    provides: {third: 0},
+    apply: function(system) {
+      system.flip(2);
+    }
+  },
+  {
+    name: 'flip 2 from 0',
+    requires: {third: 0},
+    provides: {third: 1},
+    apply: function(system) {
+      system.flip(2);
     }
   }
 ];
 
 var verifications = [
-  {
-    name: 'empty',
-    requires: {full: false},
-    apply: function(system) {
-      expect(system.length).to.equal(0);
-    }
-  },
-  {
-    name: 'full',
-    requires: {full: true},
-    apply: function(system) {
-      expect(system.length).to.equal(1);
-    }
-  }
 ];
 
 var builder = scenarioBuilder(transitions, verifications);
-builder.build(global, function() { return []; });
+builder.build(global, sut);
