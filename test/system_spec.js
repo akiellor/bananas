0d3595d506d6ca745221244658b67e1b9087cad9
Andrew Kiellor <akiellor@gmail.com>
2015-09-04 21:53:09 -0700

    Extracted transitions and verifications to different files.

--- test/system_spec.js
+++ test/system_spec.js
@@ -1,5 +1,4 @@
 var scenarioBuilder = require(__dirname + '/../lib/scenario_builder');
-var expect = require('chai').expect;
 
 function sut() {
   var state = [false, false, false];
@@ -12,125 +11,8 @@ function sut() {
   };
 }
 
-var transitions = [
-  {
-    name: 'flip 0 from initial',
-    requires: {first: undefined},
-    provides: {first: 1},
-    apply: function(system) {
-      system.flip(0);
-    }
-  },
-  {
-    name: 'flip 0 from 1',
-    requires: {first: 1},
-    provides: {first: 0},
-    apply: function(system) {
-      system.flip(0);
-    }
-  },
-  {
-    name: 'flip 0 from 0',
-    requires: {first: 0},
-    provides: {first: 1},
-    apply: function(system) {
-      system.flip(0);
-    }
-  },
-  {
-    name: 'flip 1 from initial',
-    requires: {second: undefined},
-    provides: {second: 1},
-    apply: function(system) {
-      system.flip(1);
-    }
-  },
-  {
-    name: 'flip 1 from 1',
-    requires: {second: 1},
-    provides: {second: 0},
-    apply: function(system) {
-      system.flip(1);
-    }
-  },
-  {
-    name: 'flip 1 from 0',
-    requires: {second: 0},
-    provides: {second: 1},
-    apply: function(system) {
-      system.flip(1);
-    }
-  },
-  {
-    name: 'flip 2 from initial',
-    requires: {third: undefined},
-    provides: {third: 1},
-    apply: function(system) {
-      system.flip(2);
-    }
-  },
-  {
-    name: 'flip 2 from 1',
-    requires: {third: 1},
-    provides: {third: 0},
-    apply: function(system) {
-      system.flip(2);
-    }
-  },
-  {
-    name: 'flip 2 from 0',
-    requires: {third: 0},
-    provides: {third: 1},
-    apply: function(system) {
-      system.flip(2);
-    }
-  }
-];
-
-var verifications = [
-  {
-    name: 'third is 0',
-    requires: {third: 0},
-    apply: function(system) {
-      expect(system._state[2]).to.equal(false);
-    }
-  },
-  {
-    name: 'third is 1',
-    requires: {third: 1},
-    apply: function(system) {
-      expect(system._state[2]).to.equal(true);
-    }
-  },
-  {
-    name: 'second is 1',
-    requires: {second: 1},
-    apply: function(system) {
-      expect(system._state[1]).to.equal(true);
-    }
-  },
-  {
-    name: 'second is 0',
-    requires: {second: 0},
-    apply: function(system) {
-      expect(system._state[1]).to.equal(false);
-    }
-  },
-  {
-    name: 'first is 1',
-    requires: {first: 1},
-    apply: function(system) {
-      expect(system._state[0]).to.equal(true);
-    }
-  },
-  {
-    name: 'first is 0',
-    requires: {first: 0},
-    apply: function(system) {
-      expect(system._state[0]).to.equal(false);
-    }
-  }
-];
+var transitions = require('./transitions');
+var verifications = require('./verifications'); 
 
 var builder = scenarioBuilder(transitions, verifications);
 builder.build(global, sut);
---
+++ test/transitions.js
@@ -0,0 +1,74 @@
+module.exports = [
+  {
+    name: 'flip 0 from initial',
+    requires: {first: undefined},
+    provides: {first: 1},
+    apply: function(system) {
+      system.flip(0);
+    }
+  },
+  {
+    name: 'flip 0 from 1',
+    requires: {first: 1},
+    provides: {first: 0},
+    apply: function(system) {
+      system.flip(0);
+    }
+  },
+  {
+    name: 'flip 0 from 0',
+    requires: {first: 0},
+    provides: {first: 1},
+    apply: function(system) {
+      system.flip(0);
+    }
+  },
+  {
+    name: 'flip 1 from initial',
+    requires: {second: undefined},
+    provides: {second: 1},
+    apply: function(system) {
+      system.flip(1);
+    }
+  },
+  {
+    name: 'flip 1 from 1',
+    requires: {second: 1},
+    provides: {second: 0},
+    apply: function(system) {
+      system.flip(1);
+    }
+  },
+  {
+    name: 'flip 1 from 0',
+    requires: {second: 0},
+    provides: {second: 1},
+    apply: function(system) {
+      system.flip(1);
+    }
+  },
+  {
+    name: 'flip 2 from initial',
+    requires: {third: undefined},
+    provides: {third: 1},
+    apply: function(system) {
+      system.flip(2);
+    }
+  },
+  {
+    name: 'flip 2 from 1',
+    requires: {third: 1},
+    provides: {third: 0},
+    apply: function(system) {
+      system.flip(2);
+    }
+  },
+  {
+    name: 'flip 2 from 0',
+    requires: {third: 0},
+    provides: {third: 1},
+    apply: function(system) {
+      system.flip(2);
+    }
+  }
+];
