feef7bf87947a3f7d78faed8969ad6e6a4e3cf96
Andrew Kiellor <akiellor@gmail.com>
2015-09-14 08:31:02 -0700

    Allow for defining requirements as a predicate.

--- test/build_graph_spec.js
+++ test/build_graph_spec.js
@@ -29,4 +29,34 @@ describe('build graph', function() {
     expect(graph[0].children.length).to.equal(1);
     expect(graph[0].children[0].name).to.equal('pop');
   });
+
+  it('should build a graph defined with predicates', function() {
+    var transitions = [
+      {
+        name: 'push empty',
+        requires: {full: undefined},
+        provides: {full: true},
+        apply: function(system) {
+          system.push(1);
+        }
+      },
+      {
+        name: 'pop',
+        requires: function(model) {
+          return model.full;
+        },
+        provides: {full: false},
+        apply: function(system) {
+          system.pop();
+        }
+      }
+    ];
+
+    var graph = buildGraph(transitions);
+
+    expect(graph.length).to.equal(1);
+    expect(graph[0].name).to.equal('push empty');
+    expect(graph[0].children.length).to.equal(1);
+    expect(graph[0].children[0].name).to.equal('pop');
+  });
 });
