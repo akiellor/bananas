b25bfcb5f4dfdc8314a027d83ddf5d60960df4fc
Andrew Kiellor <akiellor@gmail.com>
2015-10-19 22:06:48 -0700

    Added path_spec, fixed merging of adjacent compatible transitions.

---
+++ test/strategies/new/path_spec.js
@@ -0,0 +1,45 @@
+var Immutable = require('immutable');
+var chai = require('chai');
+var chaiImmutable = require('chai-immutable');
+chai.use(chaiImmutable);
+var expect = chai.expect;
+
+var createPath = require(__dirname + '/../../../lib/strategies/new/path');
+
+describe('path', function() {
+  it('should merge subsequent compatible transitions into same segment', function() {
+    var path = createPath()
+      .add(Immutable.fromJS({requires: {}, provides: {foo: true}}))
+      .add(Immutable.fromJS({requires: {}, provides: {bar: true}}))
+      .toRaw();
+
+    expect(path).to.equal(Immutable.List([
+      Immutable.Set([
+        Immutable.fromJS({requires: {}, provides: {foo: true}}),
+        Immutable.fromJS({requires: {}, provides: {bar: true}})
+      ])
+    ]));
+  });
+
+  it('should not merge grandchild compatible transitions', function() {
+    var path = createPath()
+      .add(Immutable.fromJS({requires: {}, provides: {foo: true}}))
+      .add(Immutable.fromJS({requires: {foo: true}, provides: {bar: true}}))
+      .add(Immutable.fromJS({requires: {bar: true}, provides: {baz: true}}))
+      .add(Immutable.fromJS({requires: {foo: true}, provides: {qux: true}}))
+      .toRaw();
+
+    expect(path).to.equal(Immutable.List([
+      Immutable.Set([
+        Immutable.fromJS({requires: {}, provides: {foo: true}}),
+      ]),
+      Immutable.Set([
+        Immutable.fromJS({requires: {foo: true}, provides: {bar: true}})
+      ]),
+      Immutable.Set([
+        Immutable.fromJS({requires: {bar: true}, provides: {baz: true}}),
+        Immutable.fromJS({requires: {foo: true}, provides: {qux: true}})
+      ]),
+    ]));
+  });
+});
