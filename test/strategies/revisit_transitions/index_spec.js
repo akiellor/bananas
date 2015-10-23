3be00a5df2a050f2a8c14cc67bd6f1b6e4ba877a
Andrew Kiellor <akiellor@gmail.com>
2015-10-22 23:41:52 -0700

    Removed names from stragtegies.

--- test/strategies/revisit_transitions/index_spec.js
+++ test/strategies/revisit_transitions/index_spec.js
@@ -17,21 +17,18 @@ describe('revisit_transitions strategy', function() {
 
     expect(testPlan.size).to.equal(3);
 
-    expect(testPlan.getIn([0, 'name'])).to.equal('0');
     expect(testPlan.getIn([0, 'steps', 0, 'name'])).to.equal('init');
     expect(testPlan.getIn([0, 'steps', 1, 'name'])).to.equal('fill half');
     expect(testPlan.getIn([0, 'steps', 2, 'name'])).to.equal('pour');
     expect(testPlan.getIn([0, 'steps', 3, 'name'])).to.equal('fill');
     expect(testPlan.getIn([0, 'steps', 4, 'name'])).to.equal('volume is one');
 
-    expect(testPlan.getIn([1, 'name'])).to.equal('1');
     expect(testPlan.getIn([1, 'steps', 0, 'name'])).to.equal('init');
     expect(testPlan.getIn([1, 'steps', 1, 'name'])).to.equal('fill half');
     expect(testPlan.getIn([1, 'steps', 2, 'name'])).to.equal('fill');
     expect(testPlan.getIn([1, 'steps', 3, 'name'])).to.equal('volume is one');
     expect(testPlan.getIn([1, 'steps', 4, 'name'])).to.equal('pour');
 
-    expect(testPlan.getIn([2, 'name'])).to.equal('2');
     expect(testPlan.getIn([2, 'steps', 0, 'name'])).to.equal('init');
     expect(testPlan.getIn([2, 'steps', 1, 'name'])).to.equal('fill');
     expect(testPlan.getIn([2, 'steps', 2, 'name'])).to.equal('volume is one');
@@ -43,7 +40,6 @@ describe('revisit_transitions strategy', function() {
     var testPlan = newStrategy(fooChainFixture.transitions, fooChainFixture.verifications);
 
     expect(testPlan.size).to.equal(1);
-    expect(testPlan.getIn([0, 'name'])).to.equal('0');
     expect(testPlan.getIn([0, 'steps', 0, 'name'])).to.equal('foo');
     expect(testPlan.getIn([0, 'steps', 1, 'name'])).to.equal('bar');
     expect(testPlan.getIn([0, 'steps', 2, 'name'])).to.equal('baz depends on foo and bar');
