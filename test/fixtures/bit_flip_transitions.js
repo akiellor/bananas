e663c4132e98bc667d4e27bcd3447b387090c51d
Andrew Kiellor <akiellor@gmail.com>
2016-02-05 12:14:01 +1100

    Simplify bitflip transitions.

--- test/fixtures/bit_flip_transitions.js
+++ test/fixtures/bit_flip_transitions.js
@@ -6,12 +6,12 @@ function noProp(name) {
 
 module.exports = [
   {
-    name: 'flip 0 from initial',
-    requires: noProp('first'),
-    provides: {first: 1},
-    apply: function(system) {
-      system.flip(0);
-    }
+    name: 'init',
+    requires: function(model) {
+      return !model.hasOwnProperty('first') && !model.hasOwnProperty('second') && !model.hasOwnProperty('third');
+    },
+    provides: {first: 0, second: 0, third: 0},
+    apply: function() {}
   },
   {
     name: 'flip 0 from 1',
@@ -30,14 +30,6 @@ module.exports = [
     }
   },
   {
-    name: 'flip 1 from initial',
-    requires: noProp('second'),
-    provides: {second: 1},
-    apply: function(system) {
-      system.flip(1);
-    }
-  },
-  {
     name: 'flip 1 from 1',
     requires: {second: 1},
     provides: {second: 0},
@@ -51,14 +43,6 @@ module.exports = [
     provides: {second: 1},
     apply: function(system) {
       system.flip(1);
-    }
-  },
-  {
-    name: 'flip 2 from initial',
-    requires: noProp('third'),
-    provides: {third: 1},
-    apply: function(system) {
-      system.flip(2);
     }
   },
   {
