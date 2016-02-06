127f3692f4b1dbce5a39a24315419eab3d28d186
Andrew Kiellor <akiellor@gmail.com>
2016-02-06 12:59:57 +1100

    Extracted interleaving logic from test plan.

---
+++ test/splice_spec.js
@@ -0,0 +1,48 @@
+var Immutable = require('immutable');
+var chai = require('chai');
+var chaiImmutable = require('chai-immutable');
+chai.use(chaiImmutable);
+var expect = chai.expect;
+var I = Immutable.fromJS;
+var splice = require('../lib/splice');
+
+describe('splice', function() {
+  it('should interleave at the appropriate locations', function() {
+    var list1 = I([
+      {
+        name: 'first',
+        provides: {first: 1}
+      },
+      {
+        name: 'third',
+        provides: function(model) {
+          model.third = 1;
+          return model;
+        }
+      }
+    ]);
+    var list2 = I([
+      {
+        name: 'second',
+        requires: function(model) {
+          return model.first && !model.third;
+        }
+      },
+      {
+        name: 'fourth',
+        requires: {third: 1}
+      }
+    ]);
+
+    var result = splice(list2, list1).map(function(item) {
+      return item.get('name');
+    });
+
+    expect(result).to.equal(I([
+      "first",
+      "second",
+      "third",
+      "fourth"
+    ]));
+  });
+});
