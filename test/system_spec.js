40d518f84bbaeefbb92b39db779c52cc5a27f571
Andrew Kiellor <akiellor@gmail.com>
2016-02-16 21:28:42 +0800

    Remove revisit transitions strategy.

--- test/system_spec.js
+++ test/system_spec.js
@@ -15,8 +15,7 @@ var fixtures = [
 
 var strategies = [
   require(__dirname + '/../lib/strategies/all_edges'),
-  require(__dirname + '/../lib/strategies/random_walk'),
-  require(__dirname + '/../lib/strategies/revisit_transitions')
+  require(__dirname + '/../lib/strategies/random_walk')
 ];
 
 describe('systems', function() {
